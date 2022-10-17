b24014e2bace9582d55a2b431c03536d719237eb
Franklin <franklin.gomez@ucuenca.edu.ec>
2022-10-16 23:40:46 -0500

    Codigo de prueba para guardar en un txt los datos muestreados

--- escritura_datos/escritura_datos.ino
+++ escritura_datos/escritura_datos.ino
@@ -1,8 +1,12 @@
 #include <SD.h>
+#define LASERPIN 8
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
 
 File myFile;
 void setup()
 {
+  pinMode(LASERPIN, OUTPUT);
   Serial.begin(9600);
   Serial.print("Iniciando SD ...");
   if (!SD.begin(4)) {
@@ -18,32 +22,27 @@ void loop()
   
   if (myFile) { 
         Serial.print("Escribiendo SD: ");
-        int sensor1 = analogRead(0);
-        int sensor2 = analogRead(1);
-        int sensor3 = analogRead(2);
-        myFile.print("Tiempo(ms)=");
-        myFile.print(millis());
-        myFile.print(", sensor1=");
-        myFile.print(sensor1);
-        myFile.print(", sensor2=");
-        myFile.print(sensor2);
-        myFile.print(", sensor3=");
-        myFile.println(sensor3);
         
+          // lee los valores
+        sensorValue = analogRead(sensorPin);
+         //int bits[]={bitRead(sensorValue,0), bitRead(sensorValue,1), bitRead(sensorValue,2), bitRead(sensorValue,3), bitRead(sensorValue,4), bitRead(sensorValue,5), bitRead(sensorValue,6), bitRead(sensorValue,7), bitRead(sensorValue,8), bitRead(sensorValue,9)};
+        int bits[]={bitRead(sensorValue,9), bitRead(sensorValue,8), bitRead(sensorValue,7), bitRead(sensorValue,6), bitRead(sensorValue,5), bitRead(sensorValue,4), bitRead(sensorValue,3), bitRead(sensorValue,2), bitRead(sensorValue,1), bitRead(sensorValue,0)};
+        
+        Serial.println(sensorValue);
+
+        myFile.print(sensorValue)
         myFile.close(); //cerramos el archivo
         
-        Serial.print("Tiempo(ms)=");
-        Serial.print(millis());
-        Serial.print(", sensor1=");
-        Serial.print(sensor1);
-        Serial.print(", sensor2=");
-        Serial.print(sensor2);
-        Serial.print(", sensor3=");
-        Serial.println(sensor3);       
                     
   
   } else {
     Serial.println("Error al abrir el archivo");
   }
-  delay(100);
+    for (int i=0; i<10; i++){
+    digitalWrite(LASERPIN, bits[i]);
+    delay(10);
+  }
+
+  digitalWrite(LASERPIN, LOW); 
+  delay(10);
 }
